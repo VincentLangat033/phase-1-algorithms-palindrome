f95d3727dcfea496cbb8f2ccc7f0d59d4b443928
Vincent Langat <kimutailangat033@gmail.com>
2022-06-17 20:59:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //create a function isPalindrome
+  // pass a string parameter to the function
+  //should return true if the string is palindrome
+  //should return false if is not
+  //deals with lowercase strings that are all letters and ignores quotattions and full stops
+  //for this test, we are going to use regular expressions
 }
 
 /* 
   Add your pseudocode here
+initialize a function called isPlandrome
+pass a parameter called str
+lowercase the string and use the regular expressions to remove unwanted strings
+
 */
 
 /*
   Add written explanation of your solution here
-*/
 
+  // str.toLowerCase() = "A man, a plan, a canal. Panama".toLowerCase() = "a man, a plan, a canal. panama"
+  // str.replace(/[\W_]/g, '') = "a man, a plan, a canal. panama".replace(/[\W_]/g, '') = "amanaplanacanalpanama"
+  // var strToLowerCase = "amanaplanacanalpanama";
+*/
+function isPalindrome(str){
+  let re = /[\W_]/g; // or var re = /[^A-Za-z0-9]/g; //the one commented out is the easiest to use
+  let strToLowercase= str.toLowerCase().replace(re, '')
+  //Now lets reverse this string
+  //first split the string then reverse then join them back 
+  let reverseString = strToLowercase.split('').reverse().join('')
+
+  //we write a code to check whether our reverseString is strictly equal to strToLowercase
+  return reverseString === strToLowercase;
+  //returns true if its a palindrome else false
+}
+
+isPalindrome("A man, a plan, a canal. Panama")
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
